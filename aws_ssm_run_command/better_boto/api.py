2ed071df2ede394b1e5b6bdf193b2b0e0a0114a7
Sanhe Hu <MacHu-GWU@users.noreply.github.com>
2023-06-18 22:21:58 -0400

    add better boto

---
+++ aws_ssm_run_command/better_boto/api.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from .aws_ssm_send_command import send_command
+from .aws_ssm_send_command import CommandInvocationStatusEnum
+from .aws_ssm_send_command import CommandInvocation
+from .aws_ssm_send_command import wait_until_command_succeeded
